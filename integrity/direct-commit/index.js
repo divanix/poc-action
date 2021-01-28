5d10f2aa537ecb08486ea71813c03679afa180a4
igibek <koishybayev.igibek@gmail.com>
2021-01-28 17:22:53 -0500

    updated index.js

--- integrity/direct-commit/index.js
+++ integrity/direct-commit/index.js
@@ -1,5 +1,7 @@
+const fs = require('fs');
 const core = require('@actions/core');
 const github = require('@actions/github');
+
 
 try {
   // `who-to-greet` input defined in action metadata file
@@ -7,9 +9,18 @@ try {
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
+  
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
+
+  console.log(`The current directory ${process.cwd()}`);
+  let contents = JSON.stringify(fs.readdirSync(process.cwd()));
+  console.log(`Current directory contents ${contents}`);
+  console.log(`The repo: ${github.context.repo}`);
+  console.log(`The workflow: ${github.context.workflow}`);
   console.log(`The event payload: ${payload}`);
+
+
 } catch (error) {
   core.setFailed(error.message);
 }
